44e3c2b44e81dfa7da4508cc3d0e0b1428042399
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-05-09 19:55:37 -0500

    se corrige SEGFAULT en REPL

--- codeblocks/CTestTestfile.cmake
+++ codeblocks/CTestTestfile.cmake
@@ -1,25 +1,25 @@
 # CMake generated Testfile for 
-# Source directory: C:/src/latino
-# Build directory: C:/src/latino/codeblocks
+# Source directory: /home/primi/src/latino
+# Build directory: /home/primi/src/latino/codeblocks
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(test "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/00-vacio.lat")
-add_test(test1 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/01-comentarios.lat")
-add_test(test2 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/02-hola.lat")
-add_test(test3 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/03-variables.lat")
-add_test(test4 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/04-expresiones.lat")
-add_test(test5 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/05-si.lat")
-add_test(test6 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/06-mientras.lat")
-add_test(test7 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/07-hacer.lat")
-add_test(test9 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/09-desde.lat")
-add_test(test10 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/10-funciones.lat")
-add_test(test11 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/11-fibonacci.lat")
-add_test(test12 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/12-factorial.lat")
-add_test(test16 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/16-funciones_cadena.lat")
-add_test(test17 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/17-escribir_archivo.lat")
-add_test(test18 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/18-leer_archivo.lat")
-add_test(test19 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/19-operadores_logicos.lat")
-add_test(test20 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/20-operadores_comparacion.lat")
-add_test(test21 "C:/src/latino/codeblocks/bin/latino.exe" "ejemplos/21-operador_ternario.lat")
-subdirs(src)
+ADD_TEST(test "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/00-vacio.lat")
+ADD_TEST(test1 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/01-comentarios.lat")
+ADD_TEST(test2 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/02-hola.lat")
+ADD_TEST(test3 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/03-variables.lat")
+ADD_TEST(test4 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/04-expresiones.lat")
+ADD_TEST(test5 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/05-si.lat")
+ADD_TEST(test6 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/06-mientras.lat")
+ADD_TEST(test7 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/07-hacer.lat")
+ADD_TEST(test9 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/09-desde.lat")
+ADD_TEST(test10 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/10-funciones.lat")
+ADD_TEST(test11 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/11-fibonacci.lat")
+ADD_TEST(test12 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/12-factorial.lat")
+ADD_TEST(test16 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/16-funciones_cadena.lat")
+ADD_TEST(test17 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/17-escribir_archivo.lat")
+ADD_TEST(test18 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/18-leer_archivo.lat")
+ADD_TEST(test19 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/19-operadores_logicos.lat")
+ADD_TEST(test20 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/20-operadores_comparacion.lat")
+ADD_TEST(test21 "/home/primi/src/latino/codeblocks/bin/latino" "ejemplos/21-operador_ternario.lat")
+SUBDIRS(src)
