83cf3eec9d6934bec185675401f9fe6a5e25b39a
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-07-08 11:37:03 -0500

    se corrige error en parse.y

--- src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/primi/src/latino/src
-# Build directory: /home/primi/src/latino/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test "/home/primi/src/latino/bin/latino" "-v")
-SUBDIRS(linenoise)
