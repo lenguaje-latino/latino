b8fce4154f207639658608b08e7a8eebc5bd1393
root <root@vps273405.ovh.net>
2016-07-12 03:53:45 +0200

    Se lanza beta 2 del editor y corrige errores

--- VBCode/Lat/Form1.Designer.vb
+++ VBCode/Lat/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
@@ -42,16 +43,24 @@ Partial Class Form1
         Me.RehacerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ToolStripMenuItem4 = New System.Windows.Forms.ToolStripSeparator()
         Me.SeleccionarTodoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopiarTodoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.LimpiarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.FormatoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.FuenteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.MásToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HerramientasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ImprimirFechaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LineasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TotalToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AcercaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GitHubToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SoporteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SitioWebToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.FontDialog1 = New System.Windows.Forms.FontDialog()
         Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'RichTextBox1
@@ -59,19 +68,19 @@ Partial Class Form1
         Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.RichTextBox1.Font = New System.Drawing.Font("Times New Roman", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RichTextBox1.Location = New System.Drawing.Point(0, 27)
+        Me.RichTextBox1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(34, 27)
         Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(523, 431)
+        Me.RichTextBox1.Size = New System.Drawing.Size(508, 462)
         Me.RichTextBox1.TabIndex = 0
         Me.RichTextBox1.Text = ""
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem, Me.EditarToolStripMenuItem, Me.FormatoToolStripMenuItem, Me.MásToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem, Me.EditarToolStripMenuItem, Me.HerramientasToolStripMenuItem, Me.AcercaToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(523, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(542, 24)
         Me.MenuStrip1.TabIndex = 1
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -129,7 +138,7 @@ Partial Class Form1
         '
         'EditarToolStripMenuItem
         '
-        Me.EditarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopiarToolStripMenuItem, Me.CortarToolStripMenuItem, Me.PegarToolStripMenuItem, Me.ToolStripMenuItem3, Me.DeshacerToolStripMenuItem, Me.RehacerToolStripMenuItem, Me.ToolStripMenuItem4, Me.SeleccionarTodoToolStripMenuItem, Me.LimpiarToolStripMenuItem})
+        Me.EditarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopiarToolStripMenuItem, Me.CortarToolStripMenuItem, Me.PegarToolStripMenuItem, Me.ToolStripMenuItem3, Me.DeshacerToolStripMenuItem, Me.RehacerToolStripMenuItem, Me.ToolStripMenuItem4, Me.SeleccionarTodoToolStripMenuItem, Me.CopiarTodoToolStripMenuItem, Me.LimpiarToolStripMenuItem})
         Me.EditarToolStripMenuItem.Name = "EditarToolStripMenuItem"
         Me.EditarToolStripMenuItem.Size = New System.Drawing.Size(49, 20)
         Me.EditarToolStripMenuItem.Text = "Editar"
@@ -186,6 +195,12 @@ Partial Class Form1
         Me.SeleccionarTodoToolStripMenuItem.Size = New System.Drawing.Size(204, 22)
         Me.SeleccionarTodoToolStripMenuItem.Text = "Seleccionar todo"
         '
+        'CopiarTodoToolStripMenuItem
+        '
+        Me.CopiarTodoToolStripMenuItem.Name = "CopiarTodoToolStripMenuItem"
+        Me.CopiarTodoToolStripMenuItem.Size = New System.Drawing.Size(204, 22)
+        Me.CopiarTodoToolStripMenuItem.Text = "Copiar todo"
+        '
         'LimpiarToolStripMenuItem
         '
         Me.LimpiarToolStripMenuItem.Name = "LimpiarToolStripMenuItem"
@@ -193,32 +208,57 @@ Partial Class Form1
         Me.LimpiarToolStripMenuItem.Size = New System.Drawing.Size(204, 22)
         Me.LimpiarToolStripMenuItem.Text = "Limpiar"
         '
-        'FormatoToolStripMenuItem
+        'HerramientasToolStripMenuItem
         '
-        Me.FormatoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FuenteToolStripMenuItem})
-        Me.FormatoToolStripMenuItem.Name = "FormatoToolStripMenuItem"
-        Me.FormatoToolStripMenuItem.Size = New System.Drawing.Size(64, 20)
-        Me.FormatoToolStripMenuItem.Text = "Formato"
-        '
-        'FuenteToolStripMenuItem
-        '
-        Me.FuenteToolStripMenuItem.Name = "FuenteToolStripMenuItem"
-        Me.FuenteToolStripMenuItem.Size = New System.Drawing.Size(171, 22)
-        Me.FuenteToolStripMenuItem.Text = "Seleccionar fuente"
-        '
-        'MásToolStripMenuItem
-        '
-        Me.MásToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ImprimirFechaToolStripMenuItem})
-        Me.MásToolStripMenuItem.Name = "MásToolStripMenuItem"
-        Me.MásToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
-        Me.MásToolStripMenuItem.Text = "Más"
+        Me.HerramientasToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ImprimirFechaToolStripMenuItem, Me.LineasToolStripMenuItem})
+        Me.HerramientasToolStripMenuItem.Name = "HerramientasToolStripMenuItem"
+        Me.HerramientasToolStripMenuItem.Size = New System.Drawing.Size(90, 20)
+        Me.HerramientasToolStripMenuItem.Text = "Herramientas"
         '
         'ImprimirFechaToolStripMenuItem
         '
         Me.ImprimirFechaToolStripMenuItem.Name = "ImprimirFechaToolStripMenuItem"
-        Me.ImprimirFechaToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.D), System.Windows.Forms.Keys)
-        Me.ImprimirFechaToolStripMenuItem.Size = New System.Drawing.Size(194, 22)
+        Me.ImprimirFechaToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5
+        Me.ImprimirFechaToolStripMenuItem.Size = New System.Drawing.Size(171, 22)
         Me.ImprimirFechaToolStripMenuItem.Text = "Imprimir fecha"
+        '
+        'LineasToolStripMenuItem
+        '
+        Me.LineasToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TotalToolStripMenuItem})
+        Me.LineasToolStripMenuItem.Name = "LineasToolStripMenuItem"
+        Me.LineasToolStripMenuItem.Size = New System.Drawing.Size(171, 22)
+        Me.LineasToolStripMenuItem.Text = "Lineas"
+        '
+        'TotalToolStripMenuItem
+        '
+        Me.TotalToolStripMenuItem.Name = "TotalToolStripMenuItem"
+        Me.TotalToolStripMenuItem.Size = New System.Drawing.Size(101, 22)
+        Me.TotalToolStripMenuItem.Text = "Total"
+        '
+        'AcercaToolStripMenuItem
+        '
+        Me.AcercaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GitHubToolStripMenuItem, Me.SoporteToolStripMenuItem, Me.SitioWebToolStripMenuItem})
+        Me.AcercaToolStripMenuItem.Name = "AcercaToolStripMenuItem"
+        Me.AcercaToolStripMenuItem.Size = New System.Drawing.Size(55, 20)
+        Me.AcercaToolStripMenuItem.Text = "Acerca"
+        '
+        'GitHubToolStripMenuItem
+        '
+        Me.GitHubToolStripMenuItem.Name = "GitHubToolStripMenuItem"
+        Me.GitHubToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.GitHubToolStripMenuItem.Text = "GitHub"
+        '
+        'SoporteToolStripMenuItem
+        '
+        Me.SoporteToolStripMenuItem.Name = "SoporteToolStripMenuItem"
+        Me.SoporteToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.SoporteToolStripMenuItem.Text = "Soporte"
+        '
+        'SitioWebToolStripMenuItem
+        '
+        Me.SitioWebToolStripMenuItem.Name = "SitioWebToolStripMenuItem"
+        Me.SitioWebToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.SitioWebToolStripMenuItem.Text = "Sitio web"
         '
         'OpenFileDialog1
         '
@@ -228,11 +268,28 @@ Partial Class Form1
         '
         Me.PrintDialog1.UseEXDialog = True
         '
+        'Timer1
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackColor = System.Drawing.Color.AliceBlue
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(33, 462)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(523, 459)
+        Me.ClientSize = New System.Drawing.Size(542, 490)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -241,6 +298,7 @@ Partial Class Form1
         Me.Text = "Latino Editor - Sin título"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -250,7 +308,6 @@ Partial Class Form1
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents EditarToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents FormatoToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents NuevoToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AbrirToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents GuardarToolStripMenuItem As ToolStripMenuItem
@@ -268,10 +325,18 @@ Partial Class Form1
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
     Friend WithEvents FontDialog1 As FontDialog
     Friend WithEvents PrintDialog1 As PrintDialog
-    Friend WithEvents FuenteToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ToolStripMenuItem4 As ToolStripSeparator
     Friend WithEvents LimpiarToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents SeleccionarTodoToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents MásToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HerramientasToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ImprimirFechaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopiarTodoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents LineasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TotalToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AcercaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SitioWebToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GitHubToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SoporteToolStripMenuItem As ToolStripMenuItem
 End Class
--- VBCode/Lat/Form1.vb
+++ VBCode/Lat/Form1.vb
@@ -1,9 +1,11 @@
 ﻿Public Class Form1
+
+    Dim CARACTER As Integer
+    Private Declare Function LockWindowUpdate Lib "user32" (ByVal hWnd As Integer) As Integer
+
+
+
     Private Sub ArchivoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ArchivoToolStripMenuItem.Click
-
-    End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
@@ -34,12 +36,13 @@
         If (OpenFileDialog1.FileName = "OpenFileDialog1") Then
             OpenFileDialog1.FileName = ""
         End If
+
         OpenFileDialog1.DefaultExt = ".lat"
         OpenFileDialog1.Filter = String.Format("
 Código latino|*.lat|
 Archivo de texto|*.txt|
-Todos los archivos|*.*
-")
+Todos los archivos|*.*|
+|")
         OpenFileDialog1.Title = "Latino Editor"
         If (OpenFileDialog1.ShowDialog = DialogResult.OK) Then
             RichTextBox1.Text = My.Computer.FileSystem.ReadAllText(OpenFileDialog1.FileName,
@@ -56,7 +59,11 @@ Todos los archivos|*.*
     End Sub
 
     Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
-        Me.Close()
+        If MessageBox.Show("¿Estás seguro de que quieres salir?", "Latino Editor",
+         MessageBoxButtons.YesNo, MessageBoxIcon.Question) _
+         = DialogResult.Yes Then
+            Application.Exit()
+        End If
     End Sub
 
     Private Sub CortarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CortarToolStripMenuItem.Click
@@ -75,11 +82,6 @@ Todos los archivos|*.*
         RichTextBox1.Redo()
     End Sub
 
-    Private Sub FuenteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FuenteToolStripMenuItem.Click
-        FontDialog1.ShowDialog()
-        RichTextBox1.Font = FontDialog1.Font
-    End Sub
-
     Private Sub LimpiarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LimpiarToolStripMenuItem.Click
         RichTextBox1.Clear()
     End Sub
@@ -89,18 +91,84 @@ Todos los archivos|*.*
     End Sub
 
     Private Sub ImprimirFechaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ImprimirFechaToolStripMenuItem.Click
-        RichTextBox1.AppendText(Date.Now)
+        RichTextBox1.AppendText(Date.Now.ToString)
     End Sub
 
-    Private Sub FormatoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FormatoToolStripMenuItem.Click
+
+
+    Private Sub CopiarTodoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopiarTodoToolStripMenuItem.Click
+        RichTextBox1.SelectAll()
+        RichTextBox1.Copy()
+        RichTextBox1.DeselectAll()
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Timer1.Interval = 10
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        PictureBox1.Refresh()
+    End Sub
+
+    Private Sub PictureBox1_Paint(sender As Object, e As System.Windows.Forms.PaintEventArgs) Handles PictureBox1.Paint
+
+        CARACTER = 0
+        Dim ALTURA As Integer = RichTextBox1.GetPositionFromCharIndex(0).Y
+
+        If RichTextBox1.Lines.Length > 0 Then
+            For I = 0 To RichTextBox1.Lines.Length - 1
+                e.Graphics.DrawString(I + 1, RichTextBox1.Font, Brushes.Black, PictureBox1.Width - (e.Graphics.MeasureString(I + 1, RichTextBox1.Font).Width - 1), ALTURA)
+                CARACTER += RichTextBox1.Lines(I).Length + 1
+                ALTURA = RichTextBox1.GetPositionFromCharIndex(CARACTER).Y
+            Next
+        Else
+            e.Graphics.DrawString(1, RichTextBox1.Font, Brushes.Black, PictureBox1.Width - (e.Graphics.MeasureString(1, RichTextBox1.Font).Width - 1), ALTURA)
+        End If
+    End Sub
+
+    Private Sub TotalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TotalToolStripMenuItem.Click
+
+        Dim lines As Integer = RichTextBox1.Lines.Count
+        If lines = 0 Then
+            MsgBox("Este código no tiene lineas (" & lines & ")")
+        Else
+            MsgBox("Este código tiene " & lines & " lineas.")
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
 
     End Sub
 
-    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+    Private Sub RichTextBox1_TextChanged(sender As System.Object, e As System.EventArgs) Handles RichTextBox1.TextChanged
         RichTextBox1.Multiline = True
         RichTextBox1.AcceptsTab = True
     End Sub
 
+
+
+
+    Private Sub SitioWebToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SitioWebToolStripMenuItem.Click
+        Dim pagina As String
+        pagina = "http://lenguaje-latino.org/"
+        Shell("explorer " & pagina)
+    End Sub
+
+    Private Sub GitHubToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GitHubToolStripMenuItem.Click
+        Dim github As String
+        github = "https://github.com/primitivorm/latino"
+        Shell("explorer " & github)
+    End Sub
+
+    Private Sub SoporteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SoporteToolStripMenuItem.Click
+        Dim soporte As String
+        soporte = "https://gitter.im/primitivorm/latino"
+        Shell("explorer " & soporte)
+    End Sub
+
+    Private Sub HerramientasToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HerramientasToolStripMenuItem.Click
+
+    End Sub
+
 End Class
-
-
