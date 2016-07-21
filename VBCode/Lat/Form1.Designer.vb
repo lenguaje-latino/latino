a5b9934987922b89153e6dcc29c7ad080dffd374
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-07-20 22:12:56 -0500

    se agrega codecov.yml

--- VBCode/Lat/Form1.Designer.vb
+++ VBCode/Lat/Form1.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
