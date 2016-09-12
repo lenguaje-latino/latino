96ccb1419011be9d163399db88220303acd8565b
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-09-12 13:40:51 -0500

    version 0.6.0

--- compilar_clang.sh
+++ compilar_clang.sh
@@ -3,6 +3,7 @@ export CC=clang
 export CXX=clang++
 export CMAKE_C_COMPILER=clang
 export CMAKE_CXX_COMPILER=clang++
-cmake -G "Unix Makefiles" .
+cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" .
+#cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" .
 make clean
 make
