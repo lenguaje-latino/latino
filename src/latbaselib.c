3ae4e73f3cb75acddd9ffa5cbd853b4ea68a03fb
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2019-06-24 19:04:05 -0500

    [fix] se corrige funcion base_leer para windows

--- src/latbaselib.c
+++ src/latbaselib.c
@@ -190,7 +190,12 @@ static void base_incluir(lat_mv *mv) {
 
 static void base_leer(lat_mv *mv) {
     char *str = calloc(1, MAX_INPUT_SIZE);
+#if (defined __WIN32__) || (defined _WIN32)
+    str = latC_leer_linea(str);
+#else
     str = latC_leer_linea(NULL);
+#endif
+
     int i = strlen(str) - 1;
     if (str[i] == '\n') {
         str[i] = '\0';
