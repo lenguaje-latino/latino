f18990a0200d875f5051ed0fbda2927db7c2909b
Primitivo Roman <cibercafe_Montero@hotmail.com>
2021-01-05 20:33:54 -0600

    se agrega Dockerfile

--- valgrind.sh
+++ valgrind.sh
@@ -1,4 +1,2 @@
 #valgrind -v --leak-check=full --track-origins=yes ./latino ../ejemplos/coverage.lat
-cd bin
-valgrind -v --leak-check=full ./latino ../ejemplos/coverage.lat
-cd ..
+valgrind -v --leak-check=full src/latino ejemplos/coverage.lat
